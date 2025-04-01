738b5da0dd2d0e4796f929544066bd9ae3c789e1
François Guillerm <guillerm.fya@orange.fr>
2025-04-01 14:16:38 +0200

    Add header and source files for grayscale conversion and resizing

---
+++ src/imgToGrayscale.h
@@ -0,0 +1,8 @@
+#ifndef IMGTOGRAYSCALE_H
+#define IMGTOGRAYSCALE_H
+
+#include "includes.h"
+
+// Déclaration du prototype des fonctions
+
+#endif
---
+++ src/resize.h
@@ -0,0 +1,8 @@
+#ifndef RESIZE_H
+#define RESIZE_H
+
+#include "includes.h"
+
+// Déclaration du prototype des fonctions
+
+#endif
