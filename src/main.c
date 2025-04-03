efdd065ed13bc0f35fe495635076c38ff83fda98
François Guillerm <guillerm.fya@orange.fr>
2025-04-03 15:43:21 +0200

    Update main.c to read JPG input and remove unused file operations

--- src/main.c
+++ src/main.c
@@ -2,19 +2,17 @@
 
 int main(int argc, char** argv) {
 
-    FILE* inputImage = fopen("data/sample1.bmp", "r");
-    FILE* grayscaleImage = fopen("data/image_gray.bmp", "w+");
+    FILE* inputImage = fopen("data/test.jpg", "rb");
     FILE* asciiImage = fopen("data/image.txt", "w+");
-    FILE* resizedImage = fopen("data/small.bmp", "w+");
 
     if(inputImage == NULL) {
         printf("File does not exist\n");
     }
+    
+    imgToAscii2(inputImage, asciiImage);
 
     fclose(inputImage);
-    fclose(grayscaleImage);
     fclose(asciiImage);
-    fclose(resizedImage);
 
     return 0;
 }
