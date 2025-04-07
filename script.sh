509070d8c938f39965d12db9bd9f010ad9f2df35
François Guillerm <guillerm.fya@orange.fr>
2025-04-07 11:54:53 +0200

    Enhance script.sh to clear terminal, handle errors, and transform video to images

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 
+clear
+
 # Verify that the number of arguments is correct
 if [ $# -ne 1 ]; then
     echo "The script requires exactly one argument: the YouTube video URL"
@@ -21,24 +23,43 @@ dataDir=data
 vidDir=data/vid
 imgDir=data/images
 textDir=data/textFiles
+errorDir=data/errors
 
 rm -rf $dataDir
 mkdir -p $dataDir
 mkdir -p $vidDir
 mkdir -p $imgDir
 mkdir -p $textDir
+mkdir -p $errorDir
 
 # Download the video using yt-dlp
 echo "Downloading video..."
 
 ext=mp4
-errorFile=data/errors.txt
-yt-dlp -f "bestvideo[ext=$ext]+bestaudio[ext=$ext]/best[ext=$ext]" -o "$vidDir/%(title)s.%(ext)s" "$url" > /dev/null 2>$errorFile
+ytDlpErrorFile=data/ytDlpErrors.txt
+yt-dlp -f "bestvideo[ext=$ext]+bestaudio[ext=$ext]/best[ext=$ext]" -o "$vidDir/%(title)s.%(ext)s" "$url" > /dev/null 2>$ytDlpErrorFile
 
 # Check if the download was successful
 if [ -f $vidDir/*.$ext ]; then
     echo "Video downloaded successfully."
 else
-    echo "Error downloading video. Check $errorFile for details."
+    echo "Error downloading video. Check $ytDlpErrorFile for details."
+    exit 1
+fi
+
+# Transform video to images
+echo "Transforming video to images..."
+
+fps=1
+ffmpegErrorFile=data/ffmpegErrors.txt
+ffmpeg -i "$vidDir/"*.$ext -vf fps=$fps "$imgDir/%06d.jpg" > /dev/null 2>$ffmpegErrorFile
+
+# Check if the transformation was successful
+testFile=$imgDir/000001.jpg
+
+if [ -f $testFile ]; then
+    echo "Video transformed to images successfully."
+else
+    echo "Error transforming video to images. Check $ffmpegErrorFile for details."
     exit 1
 fi
